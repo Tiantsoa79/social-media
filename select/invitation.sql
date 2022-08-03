c7d9c72c94fe3b68b874c729668a1e8338141acb
Tiantsoa79 <103513020+Tiantsoa79@users.noreply.github.com>
2022-08-03 22:19:48 +0300

    Update invitation.sql

--- select/invitation.sql
+++ select/invitation.sql
@@ -1,6 +1,8 @@
-select receiver.first_name 
+--Pour un certain compte avec id=1, affiche toutes les invitations non acceptees qu'il a envoye
+
+select destinator.first_name 
 from send_invite 
-inner join account as receiver 
-     on receiver.id_account=send_invite.id_account_receiver
+inner join account as destinator 
+      on destinator.id_account=send_invite.id_account_receiver
 where send_invite.is_accepted = false 
-     and send_invite.id_account_sender=1;
+      and send_invite.id_account_sender=1;
